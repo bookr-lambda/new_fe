e17c5d14509fed5ae9d872e18fd09bddecf0ed2d
Ash <ashley.cruz.crespo@gmail.com>
2019-07-10 13:48:28 -0400

    fixing bugs on the login

---
+++ src/components/LoginActions.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+
+export const LOGIN_START = "LOGIN_START";
+export const LOGIN_SUCCESSFUL = "LOGIN_SUCCESSFUL";
+export const LOGIN_FAILED = "LOGIN_FAILED";
+
+export const login = creds => dispatch => {
+  dispatch({ type: LOGIN_START });
+  return axios
+    .post("http://", creds)
+    .then(res => {
+      localStorage.setItem(
+        "token",
+        res.data.payload,
+        dispatch({ type: LOGIN_SUCCESSFUL, payload: res.data.payload })
+      );
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+export const FETCH_USER_START = "FETCH_USER_START";
+export const FETCH_USER_SUCCESSFUL = "FETCH_USER_SUCCESSFUL";
+export const FETCH_USER_FAILED = "FETCH_USER_FAILED";
+
+export const getUser = () => dispatch => {
+  dispatch({ type: FETCH_USER_START });
+  axios
+    .get("http://", {
+      headers: {
+        Authorization: localStorage.getItem("token"),
+        "Content-Type": "application/json"
+      }
+    })
+    .then(res => {
+      console.log(res);
+      dispatch({ type: FETCH_USER_SUCCESSFUL, payload: res.data });
+    })
+    .catch(err => console.log(err));
+};
+
+export const ADD_USER_START = "ADD_USER_START";
+export const ADD_USER_SUCCESS = "ADD_USER_SUCCESS";
+export const ADD_USER_FAILED = "ADD_USER_FAILED";
+
+export const addUser = signUp => dispatch => {
+  dispatch({ type: ADD_USER_START });
+  return axios
+    .post("http://", signUp)
+    .then(res => {
+      console.log(res);
+      dispatch({ type: ADD_USER_SUCCESS, payload: res.data });
+    })
+    .catch(err => console.log(err));
+};
