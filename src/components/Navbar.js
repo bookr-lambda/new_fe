8c295a90b32b965252a6af4efe8def3a04082e47
Thomas-Tuttle <48684599+Thomas-Tuttle@users.noreply.github.com>
2019-07-10 17:45:00 -0400

    Nav bar menu items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,82 +1,3 @@
-// import React from "react";
-// import CssBaseline from "@material-ui/core/CssBaseline";
-// import AppBar from "@material-ui/core/AppBar";
-// import Toolbar from "@material-ui/core/Toolbar";
-// import Typography from "@material-ui/core/Typography";
-// import Grid from "@material-ui/core/Grid";
-// import InputBase from '@material-ui/core/InputBase';
-// // import SearchIcon from '@material-ui/icons/Search';
-
-// import { makeStyles } from "@material-ui/core/styles";
-
-// function TabContainer(props) {
-//   return (
-//     <Typography component="div" style={{ padding: 8 * 3 }}>
-//       {props.children}
-//     </Typography>
-//   );
-// }
-
-// const useStyles = makeStyles(theme => ({
-//   "@global": {
-//     body: {
-//       backgroundColor: "#AAAAAA"
-//     },
-//     ul: {
-//       margin: 0,
-//       padding: 0
-//     },
-//     li: {
-//       listStyle: "none"
-//     }
-//   },
-//   appBar: {
-//     borderBottom: `1px solid ${theme.palette.divider}`
-//   },
-//   toolbar: {
-//     flexWrap: "wrap"
-//   },
-//   toolbarTitle: {
-//     flexGrow: 1
-//   },
-//   link: {
-//     margin: theme.spacing(1, 1.5)
-//   }
-// }));
-
-// export default function NavBar() {
-//   const classes = useStyles();
-
-
-//   return (
-//     <div>
-//       <CssBaseline />
-//       <AppBar position="relative">
-//         <Grid container direction="row" justify="center" alignItems="center">
-//           <Grid item xs>
-//             <Toolbar>
-//               <Typography variant="h6" color="inherit">
-//                 Bookr
-//               </Typography>
-//             </Toolbar>
-//           </Grid>
-//           <Grid item xs>
-//           <div className={classes.search}>
-//             <InputBase
-//               placeholder="Search…"
-//               classes={{
-//                 root: classes.inputRoot,
-//                 input: classes.inputInput,
-//               }}
-//               inputProps={{ 'aria-label': 'Search' }}
-//             />
-//           </div>
-//           </Grid>
-//         </Grid>
-//       </AppBar>
-//     </div>
-//   );
-// }
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -84,15 +5,13 @@ import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
 import { fade, makeStyles } from '@material-ui/core/styles';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-
 import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
 import WishListIcon from '@material-ui/icons/Favorite';
 import BookIcon from '@material-ui/icons/Book';
 import GroupIcon from '@material-ui/icons/Group';
